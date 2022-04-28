639fcc05197a97a08be530fe83f251440d3d1a29
Radim Kolar <radimkolar22@gmail.com>
2022-04-28 20:20:01 +0200

    return back button refactor

--- src/app/shared/component/return-back/return-back.component.ts
+++ src/app/shared/component/return-back/return-back.component.ts
@@ -1,4 +1,7 @@
 import { Attribute, Component, Input } from '@angular/core';
+import translation from './translation.json';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-return-back',
@@ -6,8 +9,28 @@ import { Attribute, Component, Input } from '@angular/core';
   styleUrls: ['./return-back.component.scss'],
 })
 export class ReturnBackComponent {
-  // @Input() urlPath = '';
-  @Input() buttonText = '';
+  translation = translation;
 
-  constructor(@Attribute('urlPath') public urlPath: string) {}
+  /**
+   * Text in button after arrow icon.
+   */
+  @Input() buttonText = '';
+  /**
+   * Hide text and shows only arrow button.
+   */
+  @Input() showOnlyIcon = false;
+
+  constructor(
+    @Attribute('urlPath') public urlPath: string,
+    private location: Location,
+    private router: Router
+  ) {}
+
+  /**
+   * Go to page handler.
+   * Return user back to the previous page or navigate to the written path.
+   */
+  goBack() {
+    this.urlPath ? this.router.navigate([this.urlPath]) : this.location.back();
+  }
 }
