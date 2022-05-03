2a7a6a3899d9c7af56a87a97a7104cd3e33688bc
Radim Kolar <radimkolar22@gmail.com>
2022-05-03 20:19:57 +0200

    #1 - cache category service - get all categories

--- src/app/shared/service/category-api.service.ts
+++ src/app/shared/service/category-api.service.ts
@@ -1,22 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { FoodCategory } from '../model/food-category.interface';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryApiService {
-  private apiUrlPath = environment.apiUrl + '/api/v1';
+  private readonly apiUrlPath = environment.apiUrl + '/api/v1/food-category/';
+  // cached values
+  private _categories$: Observable<FoodCategory[]> | null = null;
 
   constructor(private http: HttpClient) {}
 
   /**
-   * Get collection of Category entities
+   * Get collection of Category entities.
+   *
    */
   get(): Observable<FoodCategory[]> {
-    return this.http.get<FoodCategory[]>(this.apiUrlPath + '/food-category');
+    if (!this._categories$) {
+      this._categories$ = this.http
+        .get<FoodCategory[]>(this.apiUrlPath)
+        .pipe(shareReplay(1));
+    }
+    return this._categories$;
   }
 
   /**
@@ -24,9 +33,7 @@ export class CategoryApiService {
    * @param id
    */
   getOne(id: string): Observable<FoodCategory> {
-    return this.http.get<FoodCategory>(
-      this.apiUrlPath + '/food-category/' + id
-    );
+    return this.http.get<FoodCategory>(this.apiUrlPath + id);
   }
 
   /**
@@ -34,9 +41,6 @@ export class CategoryApiService {
    * @param formBody
    */
   create(formBody: unknown): Observable<FoodCategory> {
-    return this.http.post<FoodCategory>(
-      this.apiUrlPath + '/food-category',
-      formBody
-    );
+    return this.http.post<FoodCategory>(this.apiUrlPath, formBody);
   }
 }
